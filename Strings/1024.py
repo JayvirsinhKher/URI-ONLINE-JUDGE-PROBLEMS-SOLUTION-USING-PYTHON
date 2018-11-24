e72c7caaf8e4e9973b5fc8d840bc14a791f9c643
jayveerkher <jayveerkher@gmail.com>
2018-11-24 13:12:50 +0530

    simple encryption problem using python

---
+++ Strings/1024.py
@@ -0,0 +1,16 @@
+n =int(input())
+for x in range(n):
+    a = input()
+    b = ""
+    for i in range(len(a)):
+        if (a[i]>='A' and a[i]<='Z') or (a[i]>='a' and a[i]<='z'):
+            b += chr(ord(a[i])+3)
+        else:
+            b += a[i]
+
+    b = b[-1::-1]
+    e = b[:int(len(b)/2)]
+    for j in range(int(len(b)/2),len(b)):
+            e += chr(ord(b[j])-1)
+
+    print(e)
